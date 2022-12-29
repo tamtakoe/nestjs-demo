4fd9bfa7cee7f6155c1e13876311d5f9aeec53fc
Oleg Istomin <Oleg.Istomin@infobip.com>
2022-12-29 19:45:36 +0300

    some fixes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ export class AppController {
   constructor() {}
 
   @Get('index.html')
-  getFile(@Req() req, @Res() res, next) {
+  getFile(@Req() req, @Res() res) {
     console.log('Controller response:', res);
     
     // next();
